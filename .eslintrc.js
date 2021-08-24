16e348b0bf2e7750977cf805c11063467d763655
lucasbogerd <lucas@youngones.works>
2021-08-24 16:15:56 +0200

    build(global): improve eslint config for better integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
 		es2021: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended',
+	],
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
