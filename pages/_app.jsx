f9827f714d07a787babf6871fcf9f7b07602c57d
lucasbogerd <lucas@youngones.works>
2021-08-24 16:06:19 +0200

    feat(global): customize and extend base init

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import 'tailwindcss/tailwind.css'
+
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />
+}
+
+export default MyApp
