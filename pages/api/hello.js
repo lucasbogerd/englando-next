41246a7c41e9b12ede786815fc94b37b7d68df88
Lucas Bogerd <lucas@youngones.works>
2021-11-15 17:55:43 +0100

    revert(global): remove old api

--- pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function helloAPI(req, res) {
-	res.status(200).json({ name: 'John Doe' })
-}
